1bb7b5f7b1c79ce667d37b9665106f20c237c896
Julio Prieto <112141123+julioxp029@users.noreply.github.com>
2023-06-12 04:12:20 -0400

    Commit
    Actualización de api

---
+++ README.md
@@ -0,0 +1,12 @@
+## Descripción del proyecto  
+
+AluraFlix es un Reto del programa de Oracle Next Education y Alura Latan. El cual consistio en crear una applicación web que sirva de repositorio de videos de youtube, en el cual dichos videos estarán clasificados por categorías. 
+
+## Tecnologías utilizadas
+- React
+- Styled components
+- Material UI
+- Axios
+
+## Autores
+Julio Prieto (https://github.com/julioxp029)
---
+++ src/components/Cabecera/index.jsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import { ContenidoParcial, BotonLink, Logo } from "../UI/Estilos";
+import logo from '../../assets/img/logo.svg'
+import { Link, useLocation } from "react-router-dom";
+
+const Header = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    padding: 1rem;
+    background-color: ${({ theme }) => theme.oscuro};
+    border-bottom: solid 1px ${({ theme }) => theme.primero};
+    text-align: center;
+`;
+
+const HeaderContenido = styled(ContenidoParcial)`
+    display: flex;
+    justify-content: center;
+    justify-content: space-between;
+`;
+
+const HeaderLink = styled(Link)`
+    display: flex;
+    align-items: center;
+`;
+
+
+export function Cabecera() {
+    const url = useLocation()
+
+    return (
+        <Header>
+            <HeaderContenido>
+                <HeaderLink to='/'>
+                    <Logo src={logo} alt="" />
+                </HeaderLink>
+                {url.pathname === '/' && <BotonLink tipo='lineas' color="#fff" to='/video' >Nuevo Video</BotonLink>}
+            </HeaderContenido>
+        </Header>
+    );
+}
